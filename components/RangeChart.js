4af6d5da206d01d11aeb75c07ebd3e6a7a61e149
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-03-13 11:21:53 +0000

    cool

--- components/RangeChart.js
+++ components/RangeChart.js
@@ -3,22 +3,34 @@ import dynamic from 'next/dynamic';
 import { format, getHours, getMinutes } from 'date-fns';
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
+// {
+//   name: 'quality',
+//   data: sleep.map((x) => {
+//     return {
+//       x: x.dateOfSleep,
+//       y: x.efficiency,
+// },
+
 const RangeChart = ({ sleep }) => {
   const [series, setSeries] = useState({
     series: [
       {
+        name: 'time',
         data: sleep.map((x) => {
           let start = new Date(x.startTime).setDate(2);
-          let end = new Date(x.endTime).setDate(3);
+          let end = new Date(x.endTime).setDate(2);
+          console.log(x);
+          console.log(start, end);
           if (new Date(x.startTime).getHours() > 12) {
             start = new Date(x.startTime).setDate(1);
-            end = new Date(x.endTime).setDate(3);
+            end = new Date(x.endTime).setDate(2);
           }
           console.log(x.startTime.toString(), x.endTime.toString());
           // we need a check to see if we go to bed the day before... this fucks it up
           return {
             x: x.dateOfSleep,
             y: [start, end],
+            sleepQuality: x.efficiency,
           };
         }),
       },
@@ -39,11 +51,12 @@ const RangeChart = ({ sleep }) => {
       dataLabels: {
         enabled: true,
         formatter: function (val, opts) {
+          console.log(val, opts);
           return (
             getHours(val[0]) +
             ':' +
             getMinutes(val[0]) +
-            '-' +
+            ' - ' +
             getHours(val[1]) +
             ':' +
             getMinutes(val[1])
@@ -62,6 +75,9 @@ const RangeChart = ({ sleep }) => {
       tooltip: {
         enabled: false,
       },
+      grid: {
+        show: true,
+      },
     },
   });
   return (
@@ -71,7 +87,7 @@ const RangeChart = ({ sleep }) => {
         series={series.series}
         type='rangeBar'
         height={800}
-        width={600}
+        width={1000}
       />
       <p className='text-white'>Not as useful as I'd like...</p>
     </div>
